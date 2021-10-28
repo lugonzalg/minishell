9786555115f0bb05449bceba073e0f7c303ed045
Lukas <glukas94@gmail.com>
2021-10-28 20:16:46 +0200

    pre changing io fd logic

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,21 +6,31 @@
 /*   By: lugonzal <lugonzal@student.42urduli>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/20 13:28:19 by lugonzal          #+#    #+#             */
-/*   Updated: 2021/10/26 16:17:22 by lugonzal         ###   ########.fr       */
+/*   Updated: 2021/10/28 16:08:03 by lugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H  
 
-typedef struct s_string
+typedef struct	s_string
 {
 	char	**path;
 	char	**d2_prompt;
 	char	*user;
 	char	*prompt;
 	char	*tmp;
-}
-	t_string;
+	char	*trim_file;
+}	t_string;
+
+typedef struct	s_child
+{
+	int			**fd;
+	char		*path;
+	short int	num;
+}	t_child;
+
+extern void	print_intro(void);
+extern void	set_str(t_string *str);
 
 #endif
