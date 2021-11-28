50e71773c22c2b7c7f1f31268b0d46787a8981f0
Lukas <glukas94@gmail.com>
2021-11-28 15:50:00 +0100

    actualizado

--- src/minishell/split/split_ptr.c
+++ src/minishell/split/split_ptr.c
@@ -6,7 +6,7 @@
 /*   By: lugonzal <lugonzal@student.42urduli>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 15:13:15 by lugonzal          #+#    #+#             */
-/*   Updated: 2021/11/28 02:46:41 by lugonzal         ###   ########.fr       */
+/*   Updated: 2021/11/28 15:46:13 by lugonzal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ extern char	*ft_cut(char *s, char **s_ptr, char c, size_t on)
 	j = 0;
 	while (s[i] && s[i] != c)
 	{
-		if (s[i] == '\"' || s[i] == '\"')
+		if (s[i] == '\'' || s[i] == '\"')
 		{
 			quote = s + i + 1 - on;
 			ft_memcpy(n_str + i + j, quote, ft_query_len(quote + on, *(quote - 1 + on)) + (on * 2));
@@ -120,6 +120,25 @@ extern size_t	ft_len_redir(char *s, char c)
 	return (row);
 }
 
+static void	cut_redir(char *str, char **s_ptr, char **tab, size_t *j)
+{
+	size_t	i;
+	char	redir;
+
+	redir = *str;
+	tab[(*j)] = ft_calloc(sizeof(char), ft_strlen(str));
+	i = ft_query_len(str, redir);
+	ft_memcpy(tab + (*j), str, i);
+	(*j)++;
+	if (str[i] && str[i] != 32)
+	{
+		tab[(*j)] = ft_calloc(sizeof(char), ft_strlen(str + i));
+		i += ft_query_len(str, 32);
+		ft_memcpy(tab + (*j), str, i);
+	}
+	(*s_ptr) = str + i;
+}
+
 static char	**ft_handle_tab(const char *str, char c, char **tab, size_t on)
 {
 	size_t	j;
@@ -137,6 +156,8 @@ static char	**ft_handle_tab(const char *str, char c, char **tab, size_t on)
 				free_d2(tab);
 				return (NULL);
 			}
+			if (!on && (*str == '<' || *str == '>'))
+				cut_redir((char *)str, (char **)&str, tab, &j);
 			j++;
 		}
 		while (*str && *str != c)
